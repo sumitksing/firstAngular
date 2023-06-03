f27fe886857d5618c455360757538802c14720c4
sumit dobal <sumitksing@gmail.com>
2023-06-03 23:08:07 +0530

    AngularFirst

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent {
+    serverId: number = 10;
+    serverStatus: String = 'Loading';
+
+    getServerStatus(): String {
+        return this.serverStatus;
+    }
+}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+  allowUsers: boolean = false;
+  serverStatus: string = 'No server is added.'
+  serverName: string = ''
+
+  constructor() {
+    setTimeout(() => {
+      this.allowUsers = true;
+    }, 2000)
+  }
+
+  ngOnInit() {
+
+  }
+
+  onAddServer(event: Event) {
+    this.serverStatus = 'A server has been added';
+    console.log(event)
+  }
+
+  onUpdateServer(event: Event) {
+    console.log(event)
+    this.serverName = (<HTMLInputElement>event.target).value
+  }
+}
