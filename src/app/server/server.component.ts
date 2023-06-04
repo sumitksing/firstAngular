f0c084d674919526f78d07776b0674d073aff956
sumit dobal <sumitksing@gmail.com>
2023-06-04 17:05:10 +0530

    directives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,14 +1,27 @@
+import { style } from "@angular/animations";
 import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+    .online{
+        color: white;
+    }
+    `]
 })
 export class ServerComponent {
     serverId: number = 10;
-    serverStatus: String = 'Loading';
+    serverStatus: String = 'Offline';
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline'
+    }
 
     getServerStatus(): String {
         return this.serverStatus;
+    }
+    getColor() {
+        return this.serverStatus === 'Online' ? 'green' : 'red'
     }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent {
   allowUsers: boolean = false;
   serverStatus: string = 'No server is added.'
   serverName: string = 'TestServer'
+  serverCreated: boolean = false
 
   constructor() {
     setTimeout(() => {
@@ -21,6 +22,7 @@ export class ServersComponent {
   }
 
   onAddServer(event: Event) {
+    this.serverCreated = true
     this.serverStatus = 'A server has been added!! And the name of server is ' + this.serverName;
     // console.log(event)
   }
